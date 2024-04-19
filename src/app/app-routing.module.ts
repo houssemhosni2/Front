fff44fa86efc63cce8212efd96cbedab96da5de1
houssem hsoni <houssemhosni2018@gmail.com>
2024-04-19 09:06:28 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OffresComponent } from './components/offres/offres.component';
+import { AjouterOffreComponent } from './components/ajouter-offre/ajouter-offre.component';
+import { ModifierOffreComponent } from './components/modifier-offre/modifier-offre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/offres', pathMatch: 'full' }, 
+  { path: 'offres', component: OffresComponent },
+  { path: 'ajouter-offre', component: AjouterOffreComponent },
+  { path: 'modifier-offre/:id', component: ModifierOffreComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OffresComponent } from './components/offres/offres.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AjouterOffreComponent } from './components/ajouter-offre/ajouter-offre.component';
+import { ModifierOffreComponent } from './components/modifier-offre/modifier-offre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OffresComponent,
+    AjouterOffreComponent,
+    ModifierOffreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
